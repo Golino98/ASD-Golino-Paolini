53f623497c2d3592a9410350b7f37f81aee7d439
Golino98 <giacomogolino@gmail.com>
2024-02-28 17:15:42 +0100

    aggiunta di commenti e creazione di una nuova classe

--- src/main/java/it/asd/golino/paolini/classi/ReachGoal.java
+++ src/main/java/it/asd/golino/paolini/classi/ReachGoal.java
@@ -8,20 +8,31 @@ import java.util.*;
 
 public class ReachGoal {
 
+    // Lista globale degli agenti che hanno raggiunto l'obiettivo
     private static final ArrayList<Agente> sigma = new ArrayList<>();
 
+    /**
+     * Calcola il percorso ottimale da un punto di partenza a un obiettivo su una griglia utilizzando l'algoritmo A*.
+     *
+     * @param G       Grafo rappresentante la griglia
+     * @param init    Cella di partenza
+     * @param goal    Cella di destinazione
+     * @param max     Massimo numero di passi consentiti
+     * @param griglia Griglia di riferimento
+     * @return Agente che rappresenta il percorso ottimale
+     */
     public static Agente calculateReachGoal(Graph<Cella, DefaultWeightedEdge> G, Cella init, Cella goal, int max, Griglia griglia) {
         boolean traversable;
         int index = 0;
 
+        // Liste per la gestione degli stati aperti e chiusi
         ArrayList<VerticeTempo> closed = new ArrayList<>(), open = new ArrayList<>();
         open.add(new VerticeTempo(init, 0));
 
+        // Lista di vertici temporali (v_t)
         ArrayList<VerticeTempo> v_t = new ArrayList<>();
 
-        // devo averli già creati precedentemente per poi andarli a prendere direttamente
-        // possibile idea, usare una coppia chiave valore dove la chiave è <v,t> ed i valori sono le varie funzioni
-        // una roba di questo tipo: https://stackoverflow.com/questions/4956844/hashmap-with-multiple-values-under-the-same-key (capire come poter definire il padre)
+        // Creazione di vertici temporali per ogni vertice e tempo
         for (int t = 0; t <= max; t++) {
             for (var vertex : G.vertexSet()) {
                 VerticeTempo verticeTempo = new VerticeTempo(vertex, t);
@@ -31,6 +42,7 @@ public class ReachGoal {
             }
         }
 
+        // Inizializzazione del vertice di partenza
         for (var v : v_t) {
             if (v.getV().toString().equalsIgnoreCase(init.toString()) && v.getT() == 0) {
                 index = v_t.indexOf(v);
@@ -38,15 +50,14 @@ public class ReachGoal {
             }
         }
         v_t.get(index).setG(0);
-
-        // 11 -  f(<init,0>) <- h(init, goal)
         v_t.get(index).setF(Calcolatore.calcolaEuristica(init, goal));
 
+        // Algoritmo A*
         while (!(open.isEmpty())) {
-            // 13 - <v,t> <- the state in Open with the lowest f-score
             var lowest_f_score_state = Collections.min(open, Comparator.comparingDouble(VerticeTempo::getF));
             open.remove(lowest_f_score_state);
             closed.add(lowest_f_score_state);
+
             if (lowest_f_score_state.getV().toString().equalsIgnoreCase(goal.toString())) {
                 var percorso = reconstructPath(init, goal, lowest_f_score_state.getP(), lowest_f_score_state.getT(), griglia);
                 sigma.add(percorso);
@@ -56,31 +67,29 @@ public class ReachGoal {
             int t = lowest_f_score_state.getT();
             index = -1;
             Cella n;
-            if (t < max) {
-                // foreach n in Adj[v]
-                for (var edge : G.edgesOf(lowest_f_score_state.getV())) {
 
+            if (t < max) {
+                for (var edge : G.edgesOf(lowest_f_score_state.getV())) {
                     if (G.getEdgeTarget(edge) != lowest_f_score_state.getV()) {
                         n = G.getEdgeTarget(edge);
                     } else {
                         n = G.getEdgeSource(edge);
                     }
 
-                    // if <n, t+1> not in Closed
                     for (var v : closed) {
                         if (v.getV().toString().equalsIgnoreCase(n.toString()) && v.getT() == t + 1) {
                             index = closed.indexOf(v);
                         }
                     }
-                    // se index = -1 allora esiste NON all'interno della lista closed
+
                     if (index == -1) {
                         traversable = true;
-                        for (Agente a : sigma) {
 
-                            // Riga 28
+                        // Controllo se la cella è già stata attraversata da altri agenti
+                        for (Agente a : sigma) {
                             if (a.getCellaPercorso(t + 1).toString().equalsIgnoreCase(closed.get(index).getV().toString()) ||
-                                (a.getCellaPercorso(t + 1).toString().equalsIgnoreCase(lowest_f_score_state.getV().toString()) &&
-                                 a.getCellaPercorso(t).toString().equalsIgnoreCase(closed.get(index).getV().toString()))) {
+                                    (a.getCellaPercorso(t + 1).toString().equalsIgnoreCase(lowest_f_score_state.getV().toString()) &&
+                                            a.getCellaPercorso(t).toString().equalsIgnoreCase(closed.get(index).getV().toString()))) {
                                 traversable = false;
                             }
                         }
@@ -94,19 +103,13 @@ public class ReachGoal {
                         }
 
                         if (n_t1 != null) {
-                            double w = 0;
-                            if (G.getEdge(lowest_f_score_state.getV(), n) != null) {
-                                w = G.getEdgeWeight(G.getEdge(lowest_f_score_state.getV(), n));
-                            } else {
-                                w = G.getEdgeWeight(G.getEdge(n, lowest_f_score_state.getV()));
-                            }
+                            double w = G.getEdge(lowest_f_score_state.getV(), n) != null ?
+                                    G.getEdgeWeight(G.getEdge(lowest_f_score_state.getV(), n)) :
+                                    G.getEdgeWeight(G.getEdge(n, lowest_f_score_state.getV()));
 
                             if (traversable) {
                                 if (lowest_f_score_state.getG() + w < n_t1.getG()) {
-                                    // P(<n, t+1>) <- <v,t>
                                     n_t1.setP(lowest_f_score_state);
-
-                                    // g(<n, t+1>) <- g(<v,t>) + w(v,n)
                                     n_t1.setG(lowest_f_score_state.getG() + w);
                                     n_t1.setF(n_t1.getG() + Calcolatore.calcolaEuristica(n, goal));
                                 }
@@ -119,12 +122,20 @@ public class ReachGoal {
                     }
                 }
             }
-
-            //RITORNO ERRORE
         }
-        return null;
+        return null; // RITORNO ERRORE
     }
 
+    /**
+     * Ricostruisce il percorso ottimale da un vertice temporale iniziale al vertice di destinazione.
+     *
+     * @param init    Cella di partenza
+     * @param goal    Cella di destinazione
+     * @param P       Vertice temporale precedente
+     * @param t       Tempo
+     * @param griglia Griglia di riferimento
+     * @return Agente rappresentante il percorso ottimale
+     */
     private static Agente reconstructPath(Cella init, Cella goal, VerticeTempo P, int t, Griglia griglia) {
         for (var a : Griglia.listaAgenti) {
             if (a.getCellaStart().toString().equalsIgnoreCase(init.toString()) && a.getCellaGoal().toString().equalsIgnoreCase(goal.toString())) {
@@ -135,6 +146,6 @@ public class ReachGoal {
                 a.setFoundBest(true);
             }
         }
-        return null;
+        return null; // Non utilizzato
     }
 }
